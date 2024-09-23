33338232b9cb14de9f88b5f30ca760a8a90702cd
YuzuneYuzuriha <git@yuzneri.net>
2024-09-23 21:49:29 +0900

    スナップショットテストを追加

---
+++ e2e/guest/top.spec.ts
@@ -0,0 +1,6 @@
+import {expect, test} from '@playwright/test';
+
+test('TOPページ', async ({page}, testInfo) => {
+    await page.goto('/');
+    await expect(page).toHaveScreenshot({fullPage : true});
+});
