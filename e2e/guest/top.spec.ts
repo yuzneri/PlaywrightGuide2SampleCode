a7ad5a62e83ccf2c738fa0544477669524aeb468
YuzuneYuzuriha <git@yuzneri.net>
2024-10-06 22:56:05 +0900

    モックテスト追加

--- e2e/guest/top.spec.ts
+++ e2e/guest/top.spec.ts
@@ -9,3 +9,26 @@ test('TOPページのスナップショットテスト', async ({page}, testInfo
     await page.goto('/');
     expect(await page.textContent('body')).toMatchSnapshot();
 });
+
+test('リクエストをモック', async ({page}, testInfo) => {
+    await page.route('/', async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: 'text/html',
+            body: '<!doctype html><meta charset="utf-8"><title>Mock</title><h1>Mock</h1>',
+        });
+    });
+    await page.goto('/');
+    await expect(page.getByRole('heading')).toContainText('Mock');
+});
+
+test('元のレスポンスを取得し書き換える', async ({page}, testInfo) => {
+    await page.route('/', async (route) => {
+        const response = await route.fetch();
+        const body = (await response.text()).replace(/ログイン/g, '会員メニュー');
+        await route.fulfill({response, body});
+    });
+
+    await page.goto('/');
+    await expect(page.getByRole('main')).toContainText('会員メニュー');
+});
