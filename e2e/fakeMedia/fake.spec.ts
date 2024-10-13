c9230dcb57e2c2ecc7af251b06634d2791a444f1
YuzuneYuzuriha <git@yuzneri.net>
2024-10-13 23:34:25 +0900

    元に戻す

--- e2e/fakeMedia/fake.spec.ts
+++ e2e/fakeMedia/fake.spec.ts
@@ -3,18 +3,14 @@ import {test} from '@playwright/test';
 // 外部のテストサイトを指定しているため、skipにしています。
 // 確認したい場合、.skipを削除し、テスト後に録画データを確認してください。
 
-test('カメラのテスト', async ({page}) => {
+test.skip('カメラのテスト', async ({page}) => {
     await page.goto('https://webcammictest.com/');
     await page.getByRole('button', {name: 'Start test'}).click();
     await page.waitForTimeout(5000)
 });
 
-test('マイクのテスト', async ({page}) => {
+test.skip('マイクのテスト', async ({page}) => {
     await page.goto('https://webcammictest.com/check-mic.html');
     await page.getByRole('button', {name: 'Start test'}).click();
     await page.waitForTimeout(5000)
-    await page.locator('#preview-html')
-        .contentFrame()
-        .getByRole('link', {name: 'Verify Email Address'})
-        .click();
 });
