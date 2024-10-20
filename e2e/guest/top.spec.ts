798b6f0212ab13a403a9c0c6f0b7084c1c97aefd
YuzuneYuzuriha <git@yuzneri.net>
2024-10-20 22:27:12 +0900

    リクエストとレスポンスのテスト

--- e2e/guest/top.spec.ts
+++ e2e/guest/top.spec.ts
@@ -9,3 +9,19 @@ test('TOPページのスナップショットテスト', async ({page}, testInfo
     await page.goto('/');
     expect(await page.textContent('body')).toMatchSnapshot();
 });
+
+test('background.svgのリクエストがされているか', async ({page}, testInfo) => {
+    const requestPromise = page.waitForRequest('img/background.svg');
+    await page.goto('/');
+    const request = await requestPromise;
+
+    expect(request.method()).toBe('GET');
+});
+
+test('background.svgのレスポンスがされているか', async ({page}, testInfo) => {
+    const responsePromise = page.waitForResponse('img/background.svg');
+    await page.goto('/');
+    const response = await responsePromise;
+
+    expect(response.status()).toBe(200);
+});
